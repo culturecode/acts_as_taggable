culturecode/acts_as_taggable

commit 3fcbeabfff7be2d8ed4ba59c26c2b03b4af3630a

Solidify association cache clearing
We originally relied on unspecified cache clearing behaviour when adding tags. These changes make the cache clearing
more explicit.

lib/acts_as_taggable/acts_as_taggable.rb
@@ -7,15 +7,15 @@ module ActsAsTaggable
       self.acts_as_taggable_options.reverse_merge! :output_delimiter => acts_as_taggable_options[:delimiter]
       self.acts_as_taggable_options[:types] = Array(self.acts_as_taggable_options[:types])
 
-      has_many :taggings, -> { order("#{Tagging.table_name}.id") }, :as => :taggable, :after_remove => :delete_tag_if_necessary, :dependent => :destroy, :class_name => 'ActsAsTaggable::Tagging'
-      has_many :tags, :through => :taggings, :class_name => 'ActsAsTaggable::Tag', :after_add => :reset_scoped_associations
+      has_many :taggings, -> { order("#{Tagging.table_name}.id") }, :as => :taggable, :after_add => :reset_associations_after_taggings_change, :after_remove => :delete_tag_if_necessary, :dependent => :destroy, :class_name => 'ActsAsTaggable::Tagging'
+      has_many :tags, :through => :taggings, :class_name => 'ActsAsTaggable::Tag', :after_add => :reset_associations_after_tag_change
 
       extend ClassMethods
       include InstanceMethods
 
       self.acts_as_taggable_options[:types].each do |tag_type|
-        has_many :"#{tag_type}_taggings", -> { joins(:tag).order("#{Tagging.table_name}.id").where(Tag.table_name => {:tag_type => tag_type}) }, :as => :taggable, :after_remove => :delete_tag_if_necessary, :class_name => 'ActsAsTaggable::Tagging'
-        has_many :"#{tag_type}_tags", -> { where(:tag_type => tag_type) }, :through => :taggings, :source => :tag, :class_name => 'ActsAsTaggable::Tag', :after_add => :reset_associations
+        has_many :"#{tag_type}_taggings", -> { joins(:tag).order("#{Tagging.table_name}.id").where(Tag.table_name => {:tag_type => tag_type}) }, :as => :taggable, :after_add => :reset_associations_after_taggings_change, :after_remove => :delete_tag_if_necessary, :class_name => 'ActsAsTaggable::Tagging'
+        has_many :"#{tag_type}_tags", -> { where(:tag_type => tag_type) }, :through => :taggings, :source => :tag, :class_name => 'ActsAsTaggable::Tag', :after_add => :reset_associations_after_tag_change
 
         metaclass = class << self; self; end
         HelperMethods.scope_class_methods(metaclass, tag_type)
@@ -121,18 +121,20 @@ module ActsAsTaggable
     private
 
     def delete_tag_if_necessary(tagging)
+      reset_associations_after_taggings_change(tagging)
       self.class.tags.where(:id => tagging.tag_id).destroy_all if acts_as_taggable_options[:remove_tag_if_empty] && tagging.tag.taggings.count == 0
     end
 
-    def reset_scoped_associations(record)
-      return unless record.tag_type
-      send("#{record.tag_type}_taggings").reset
-      send("#{record.tag_type}_tags").reset
-    end
-
-    def reset_associations(record)
+    def reset_associations_after_tag_change(tag)
       send(:taggings).reset
       send(:tags).reset
+      return unless tag.tag_type
+      send("#{tag.tag_type}_taggings").reset
+      send("#{tag.tag_type}_tags").reset
+    end
+
+    def reset_associations_after_taggings_change(tagging)
+      reset_associations_after_tag_change(tagging.tag)
     end
   end
 

commit 22a6814ade4d0790f94ccf3085cd66dbe6ce26d5

Fix associations being cleared before a record is initially saved
This prevented creating new records with tags, e.g.

`Record.create!(:tag_string => "haida, carving")`

since the `after_add` hook remove the tagging before it was persisted.

This is a regression I introduced in https://github.com/culturecode/acts_as_taggable/commit/3fcbeabfff7be2d8ed4ba59c26c2b03b4af3630a

The only piece of that old commit that was actually relevant was to reset the association cache when a tag was deleted.

lib/acts_as_taggable/acts_as_taggable.rb
@@ -7,15 +7,15 @@ module ActsAsTaggable
       self.acts_as_taggable_options.reverse_merge! :output_delimiter => acts_as_taggable_options[:delimiter]
       self.acts_as_taggable_options[:types] = Array(self.acts_as_taggable_options[:types])
 
-      has_many :taggings, -> { order("#{Tagging.table_name}.id") }, :as => :taggable, :after_add => :reset_associations_after_taggings_change, :after_remove => :delete_tag_if_necessary, :dependent => :destroy, :class_name => 'ActsAsTaggable::Tagging'
-      has_many :tags, :through => :taggings, :class_name => 'ActsAsTaggable::Tag', :after_add => :reset_associations_after_tag_change
+      has_many :taggings, -> { order("#{Tagging.table_name}.id") }, :as => :taggable, :after_remove => :delete_tag_if_necessary, :dependent => :destroy, :class_name => 'ActsAsTaggable::Tagging'
+      has_many :tags, :through => :taggings, :class_name => 'ActsAsTaggable::Tag', :after_add => :reset_scoped_associations
 
       extend ClassMethods
       include InstanceMethods
 
       self.acts_as_taggable_options[:types].each do |tag_type|
-        has_many :"#{tag_type}_taggings", -> { joins(:tag).order("#{Tagging.table_name}.id").where(Tag.table_name => {:tag_type => tag_type}) }, :as => :taggable, :after_add => :reset_associations_after_taggings_change, :after_remove => :delete_tag_if_necessary, :class_name => 'ActsAsTaggable::Tagging'
-        has_many :"#{tag_type}_tags", -> { where(:tag_type => tag_type) }, :through => :taggings, :source => :tag, :class_name => 'ActsAsTaggable::Tag', :after_add => :reset_associations_after_tag_change
+        has_many :"#{tag_type}_taggings", -> { joins(:tag).order("#{Tagging.table_name}.id").where(Tag.table_name => {:tag_type => tag_type}) }, :as => :taggable, :after_add => :reset_associations, :after_remove => :delete_tag_if_necessary, :class_name => 'ActsAsTaggable::Tagging'
+        has_many :"#{tag_type}_tags", -> { where(:tag_type => tag_type) }, :through => :taggings, :source => :tag, :class_name => 'ActsAsTaggable::Tag', :after_add => :reset_associations
 
         metaclass = class << self; self; end
         HelperMethods.scope_class_methods(metaclass, tag_type)
@@ -128,20 +128,23 @@ module ActsAsTaggable
     private
 
     def delete_tag_if_necessary(tagging)
-      reset_associations_after_taggings_change(tagging)
+      if tagging.tag.tag_type
+        reset_associations(tagging.tag)
+      else
+        reset_scoped_associations(tagging.tag)
+      end
       self.class.tags.where(:id => tagging.tag_id).destroy_all if acts_as_taggable_options[:remove_tag_if_empty] && tagging.tag.taggings.count == 0
     end
 
-    def reset_associations_after_tag_change(tag)
+    def reset_associations(_)
       send(:taggings).reset
       send(:tags).reset
+    end
+
+    def reset_scoped_associations(tag)
       return unless tag.tag_type
       send("#{tag.tag_type}_taggings").reset
       send("#{tag.tag_type}_tags").reset
-    end
-
-    def reset_associations_after_taggings_change(tagging)
-      reset_associations_after_tag_change(tagging.tag)
     end
   end
 

spec/lib/acts_as_taggable_spec.rb
@@ -142,6 +142,21 @@ describe 'acts_as_taggable' do
       expect(record.tags.collect(&:name)).to contain_exactly('red', 'green')
     end
 
+    it 'creates with an array of strings and sets the tags tag match' do
+      record = klass.create!(:tag_names => ['red', 'green'])
+      expect(record.tags.collect(&:name)).to contain_exactly('red', 'green')
+    end
+
+    it 'updates with different tags' do
+      record.update!(:tag_names => ['blue', 'green'])
+      expect(record.tags.collect(&:name)).to contain_exactly('blue', 'green')
+    end
+
+    it 'updates with no tags' do
+      record.update!(:tag_names => [])
+      expect(record.tags.collect(&:name)).to be_empty
+    end
+
     it 'ignores empty strings' do
       record.tag_names = ['', 'green']
       expect(record.tags.collect(&:name)).to contain_exactly('green')

spec/lib/tag_scoping_spec.rb
@@ -74,6 +74,14 @@ describe 'acts_as_taggable' do
       record.material_tags = [metal]
       expect(record.tags).to contain_exactly(red, metal)
     end
+
+    it 'resets the unscoped tags association when tags are removed' do
+      record.material_tags = [metal]
+      expect(record.tags).to contain_exactly(metal)
+
+      record.material_taggings = []
+      expect(record.tags).to be_empty
+    end
   end
 
   describe '#tag_type_taggings' do
@@ -91,7 +99,6 @@ describe 'acts_as_taggable' do
       expect(record.taggings.collect(&:tag)).to contain_exactly(red, metal, wood)
     end
   end
-
 
   describe '#tag_type_tags' do
     it "returns only tags where the tag's tag type matches" do